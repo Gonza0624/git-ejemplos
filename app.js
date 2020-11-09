f48560e6b843b8e9a0277f56ed05d568530f778c
Gonza0624 <gonzalosalazar1180@gmail.com>
2020-11-09 15:57:09 -0500

    agregamos ciclo while en origin

--- app.js
+++ app.js
@@ -7,3 +7,39 @@ let numMax = (a, b) => {
 }
 
 console.log(numMax(2,8));
+
+
+let a = 10,
+b = 5;
+
+let resultado = a + b;
+
+console.log(`el resultado es ${resultado}`);
+
+
+let suma = (a, b) => {
+    return a + b;
+}
+
+console.log(`el resultado es ${suma(2, 5)}`)
+
+
+ let user = {
+    name: 'gonza',
+    lastName: 'salazar' 
+}
+
+function cambiar (user, newName, newLastName) {
+    user.name = newName
+    user.lastName = newLastName
+}
+
+console.log(cambiar(user, 'brayan', 'salazar'))
+
+
+let i = 0;
+
+    while (i <= 10) {
+        console.log(i);
+        i++;
+    }
